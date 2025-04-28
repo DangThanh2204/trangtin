ad95f825ca2c924f511348f45839453dc6b894c4
DHoangThanh <dhthanh2004@gmail.com>
2025-04-28 22:53:39 +0700

    Sua code fix lan cuoi

--- index.js
+++ index.js
@@ -55,10 +55,6 @@ app.use('/chude', chudeRouter);
 app.use('/taikhoan', taikhoanRouter);
 app.use('/baiviet', baivietRouter);
 
-//app.listen(3000, () => {
-//	console.log('Server is running at http://127.0.0.1:3000');
-//});
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+app.listen(3000, () => {
+	console.log('Server is running at http://127.0.0.1:3000');
 });
